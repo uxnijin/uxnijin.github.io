bb47f5a06eeb60330c47104fb4bb247da0f546d8
Nijin <nijuvenghat550649@gmail.com>
2025-02-24 18:01:22 +0530

    content needs to be changed

--- data.js
+++ data.js
@@ -24,7 +24,7 @@ const portfolioData = {
     works: [
         { 
             id: "work0",
-            title: "Designing for AI Engineers: UI patterns you need to know",
+            title: "Designing for AI Engineers: UI patterns you need to know for AI Engineers: UI patterns you need to know",
             image: "assets/work1.jpg",
             content: {
                 featuredImage: "assets/work1.jpg",
@@ -57,52 +57,12 @@ const portfolioData = {
                     }
                 ]
             }
-        },
-        { 
-            id: "work2",
-            title: "Designing for AI Engineers: UI patterns you need to know",
-            image: "assets/work1.jpg",
-            content: {
-                featuredImage: "assets/work1-featured.jpg",
-                blocks: [
-                    {
-                        text: "This project involved creating UI patterns tailored for AI engineers, focusing on usability and efficiency.",
-                        images: ["assets/work1-img1.jpg", "assets/work1-img2.jpg"]
-                    },
-                    {
-                        text: "We iterated on these designs to ensure they met the needs of a fast-paced development team.",
-                        images: ["assets/work1-img3.jpg"]
-                    }
-                ]
-            }
-        },
-        { 
-            id: "work3",
-            title: "Designing for AI Engineers: UI patterns you need to know",
-            image: "assets/work1.jpg",
-            content: {
-                featuredImage: "assets/work1.jpg",
-                blocks: [
-                    {
-                        text: "As the Head of Design at Modular AI, my team is going from 0 to 1 on a lot of AI products and workflows this year. Even though I’ve been designing for developers for years, I’ve personally had to quickly ramp up on some AI specific topics and build out our personas, so I thought I’d share a few best practices and top references I’ve established for myself",
-                        images: ["assets/writing1.png", "assets/writing1.png"]
-                    },
-                    {
-                        text: "So with those principles in mind here are some key interfaces that my team has released this year. Keep in mind my team is 4 people (2 designers & 2 developers), and we’re a fast-paced start-up. Our designs are all released as MVPs at first, then iterated once live, and often built off of existing systems to get from 0 to 1 and get real external validation as fast as possible. All of the designs in this post were created by myself and Will Rust together. Design is a team sport.",
-                        images: ["assets/writing2.jpg", "assets/writing1.png"] // you can add up to 2
-                    },
-                    {
-                        text: "<em>I’m really excited to build so many more workflows and share them. I hope to focus on agentic workflows and cloud monitoring dashboards next.</em>",
-                        images: ["assets/writing1.png", "assets/writing1.png"]
-                    }
-                ]
-            }
         }
     ],
     writings: [
         { 
             id: "writing1", 
-            title: "Designing for AI Engineers: UI patterns you need to know", 
+            title: "Designing for AI Engineers: UI patterns you need to know for AI Engineers: UI patterns you need to know", 
             thumbnail: "assets/writings.png",
             content: {
                 featuredImage: "assets/writing1.png",
